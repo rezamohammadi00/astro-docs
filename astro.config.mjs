8d3c7927f2156bca0a2fdbe5e762b4e509922a3d
Rez <mohammadireza7979.com>
2024-08-28 03:58:10 +0330

    SSG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+//Most frameworks define a way for us to specify our project-specific configurations.
+
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()]
+});
